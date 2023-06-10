6c9d6bd86ed9917af9762d513f38d24a95fdb3f2
JimSweeneyJr <jimsweeney566@gmail.com>
2023-06-10 07:07:44 -0400

    add ons

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -6,17 +6,20 @@ import { NavLink } from "react-router-dom";
 const WorkCard = (props) => {
   return (
     <div className="project-card">
+      <div className="project-card-hover">
       <img src={props.imgsrc} alt="image" />
-      <h2 className="project-title">{props.title}</h2>
+      <h2 className="project-title">{props.details}</h2>
+      </div>
       <div className="pro-details">
         <p>{props.text}</p>
+        <p>{props.details}</p>
         <div className="pro-btns">
-          <NavLink to={props.view} className="btn">
-            View
+          <NavLink to={props.view} className="nav-link-btn">
+          {props.title}
           </NavLink>
-          <NavLink to="url.com" className="btn">
+          {/* <NavLink to="url.com" className="btn">
             Source
-          </NavLink>
+          </NavLink> */}
         </div>
       </div>
     </div>
@@ -24,3 +27,6 @@ const WorkCard = (props) => {
 };
 
 export default WorkCard;
+
+
+{/* <h2 className="project-title">{props.title}</h2> */}
--- src/components/WorkCradData.js
+++ src/components/WorkCradData.js
@@ -5,38 +5,18 @@ import pro3 from "../assets/splashscreen.png";
 const ProjectCardData = [
   {
     imgsrc: pro1,
-    title: "Food Blog Swipe Site",
-    text: "This is only a test!",
+    title: "Food Blog Project",
+    details: "test",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
   },
   {
     imgsrc: pro2,
-    title: "SwiftUI Master Class",
-    text: "This is only a test",
+    title: "Fruits Project",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
   },
   {
     imgsrc: pro3,
     title: "UDEMY SwiftUI Class",
-    text: "This is only test",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-  },
-  {
-    imgsrc: pro1,
-    title: "Food Blog Swipe Site",
-    text: "This is only a test!",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-  },
-  {
-    imgsrc: pro2,
-    title: "SwiftUI Master Class",
-    text: "This is only a test",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-  },
-  {
-    imgsrc: pro3,
-    title: "UDEMY SwiftUI Class",
-    text: "This is only test",
     view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
   },
 ];
